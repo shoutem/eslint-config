3187da520bd5aa5c8cd831eec7e50f311245e4b6
Lucija Tatarević <lucija.tatarevic@gmail.com>
2022-08-01 14:22:13 +0200

    Add UI components to skip no-raw-text rule
    Feature adds shoutem/UI text components to skip option of no-raw-text to prevent false warnings from this rule. All added components are using Text component directly with different styling.

--- packages/eslint-config-react-native/rules/custom.js
+++ packages/eslint-config-react-native/rules/custom.js
@@ -2,7 +2,7 @@ module.exports = {
   rules: {
     "react-native/no-unused-styles": 2,
     "react-native/no-inline-styles": 2,
-    "react-native/no-raw-text": 1,
+    "react-native/no-raw-text": [1, { "skip": ["Caption", "Heading", "Subtitle", "Title"] }],
     "react-native/no-single-element-style-arrays": 2,
     "react/jsx-filename-extension": [1, { "extensions": [".js"] }]
   },
