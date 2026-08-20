37429a69b3fc91a3ac51c2fd4c83b22e00bbb101
tomislav-arambasic <tomislav.arambasic@gmail.com>
2026-08-20 12:56:34 +0200

    update deps, remove duplicate declarations and rules that dont exist any more

--- packages/eslint-config-base/rules/custom.js
+++ packages/eslint-config-base/rules/custom.js
@@ -1,19 +1,10 @@
 module.exports = {
   rules: {
-    "max-len": [
-      2,
-      120,
-      2,
-      {
-        ignoreUrls: true,
-        ignoreComments: false,
-      },
-    ],
-    "no-empty-label": 0,
+    // arrow-body-style and prefer-arrow-callback must stay off when using
+    // eslint-plugin-prettier (auto-fix conflicts); eslint-config-prettier >=7
+    // no longer disables them itself.
     "arrow-body-style": 0,
-    "space-before-keywords": 0,
-    "space-after-keywords": 0,
-    "space-return-throw-case": 0,
+    "prefer-arrow-callback": 0,
     "no-iterator": 0,
     "no-unused-expressions": 0,
     "no-param-reassign": [2, { props: false }],
@@ -40,16 +31,6 @@ module.exports = {
     "func-names": 0,
     "no-underscore-dangle": "off",
     "class-methods-use-this": "off",
-    "comma-dangle": [
-      "error",
-      {
-        arrays: "always-multiline",
-        objects: "always-multiline",
-        imports: "always-multiline",
-        exports: "always-multiline",
-        functions: "always-multiline",
-      },
-    ],
     radix: 0,
     "linebreak-style": 0,
   },
